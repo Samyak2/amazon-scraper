be1262a9be4d3b7d2a8ff4484c1f045411ea526d
Samyak S Sarnayak <samyak201@gmail.com>
2019-09-05 20:14:03 +0530

    Updated scheduler

--- scheduled.py
+++ scheduled.py
@@ -1,10 +1,32 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval', hours=3)
-def timed_job():
+def start_scraper():
     subprocess.Popen(["scrapy", "crawl", "amazon_spider", "-o", "output.json"])
 
-sched.start()
+# All the required imports
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.combining import OrTrigger
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def start_scheduler():
+
+     # define a background schedule 
+     # Attention: you cannot use a blocking scheduler here as that will block the script from proceeding.
+     scheduler = BlockingScheduler()
+
+     # define your job trigger
+     hourse_keeping_trigger = CronTrigger(hour="*/3") #OrTrigger([CronTrigger()]) #CronTrigger(hour='12', minute='30')
+
+     # add your job
+     scheduler.add_job(func=start_scraper, trigger=hourse_keeping_trigger)
+
+     # start the scheduler
+     scheduler.start()
+
+
+def run():
+    start_scheduler()
+
+if __name__ == '__main__':
+    run()
